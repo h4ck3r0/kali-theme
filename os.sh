4ed1b20d480decc5b6d111c5a5fd41b4a14b5447
H4Ck3R <46929618+h4ck3r0@users.noreply.github.com>
2020-11-16 07:24:01 -0500

    Update os.sh

--- os.sh
+++ os.sh
@@ -32,7 +32,7 @@ wr () {
                                                }
                                                3line() {
                                                        sudo chsh -s zsh 
-                                                       cd ~/Kali-theme/.object ; bash .2.sh                                                       
+                                                       cd ~/kali-theme ; bash .2.sh                                                      
                                                        cd ~/kali-theme ; bash os.sh
                                                        
                                                          }
